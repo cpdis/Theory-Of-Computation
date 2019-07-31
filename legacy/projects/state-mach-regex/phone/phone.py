65f30103cc314ce809a0bdd19454f912eb22df72
Colin Dismuke <colin.dismuke@gmail.com>
2019-07-30 21:52:07 -0500

    Added regex and comments explaining

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -4,7 +4,30 @@ import re  # module for processing regular expressions https://docs.python.org/3
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-regex = ''
+regex = '\+?([\d]{0,3})[-\s]?\(?(\d{3})\)?\s?\-?(\d{3})\s?\-?(\d{4})'
+
+''' 
+\+ --> Looks for the + character
+? --> Makes the preceding regex optional
+([\d]{0,3}) --> Matches between 0 and 3 digits (country code)
+[-\s] --> Matches the - character and any whitespace
+? --> Makes the preceding regex optional
+\( --> Escapes the (
+? --> Makes the preceding regex optional
+(\d{3}) --> Matches 3 digits
+\) --> Escapes the )
+? --> Makes the preceding regex optional
+\s --> Matches any whitespace
+? --> Makes the preceding regex optional
+\- --> Matches the - character
+? --> Makes the preceding regex optional
+(\d{3}) --> Matches 3 digits
+\s --> Matches any whitespace
+? --> Makes the preceding regex optional
+\- --> Matches the - character
+? --> Makes the preceding regex optional
+(\d{4}) --> Matches 4 digits
+'''
 
 while line != "exit":
     # TODO Find matches
@@ -16,7 +39,7 @@ while line != "exit":
 
     # TODO Else, break number up into area code, prefix, and suffic
     else:
-        print('Country code: %s \n Area code: %s \n Prefix: %s \n Suffix: %s' %
+        print('Country code: %s \nArea code: %s \nPrefix: %s \nSuffix: %s' %
               (matches[1], matches[2], matches[3], matches[4]))
 
     # As a stretch goal, you can modify your regex to search for country codes
