02fac68e42c940e3aab5536d7681994b1dd69e6d
Colin Dismuke <colin.dismuke@gmail.com>
2019-07-30 21:35:25 -0500

    Added logic to print out correctly split up phone number

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -11,8 +11,13 @@ while line != "exit":
     matches = re.search(regex, line)
 
     # TODO If no match found, print that no number was found
+    if not matches:
+        print('❗ This is not a valid phone number.')
 
     # TODO Else, break number up into area code, prefix, and suffic
+    else:
+        print('Country code: %s \n Area code: %s \n Prefix: %s \n Suffix: %s' %
+              (matches[1], matches[2], matches[3], matches[4]))
 
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
